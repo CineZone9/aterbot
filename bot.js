260ce04f9c22c01248e28371e524e9775ca257fe
CineZone9 <eriknoel_ahlgrimm@icloud.com>
2026-04-22 18:14:42 +0200

    bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const { pathfinder, Movements, goals } = require('mineflayer-pathfinder');
 const config = {
   host: process.env.MC_HOST || 'RobGie2010-sLMB.aternos.me',
   port: parseInt(process.env.MC_PORT) || 27982,
-  username: process.env.MC_USERNAME || 'Player-AI',
+  username: process.env.MC_USERNAME || 'PlayerAI',
   password: process.env.MC_PASSWORD || undefined, // nur für Premium-Accounts
   version: process.env.MC_VERSION || false, // false = auto-detect
   auth: process.env.MC_AUTH || 'offline', // 'offline' oder 'microsoft'
